6d3b51cdc1385dcd2ab34da759af888cd4d6103e
Stanislav <solarcoaster.st@gmail.com>
2024-04-10 17:20:46 +0300

    created beta version of 'Filter view Builder'

--- src/components/UI/choose-vendors-modal/choose-vendors-modal.component.jsx
+++ src/components/UI/choose-vendors-modal/choose-vendors-modal.component.jsx
@@ -2,136 +2,46 @@ import React, { useState } from "react";
 import { Box, Divider, Stack } from "@mui/material";
 import ArrowForwardRoundedIcon from "@mui/icons-material/ArrowForwardRounded";
 
-import CustomModal from "../modal/custom-modal.component";
-import BadgeButton from "../buttons/badge-button/badge-button.component";
+//import type { RuleGroupType } from 'react-querybuilder';
+import { QueryBuilder } from "react-querybuilder";
+import { fields } from "./fields";
+import "react-querybuilder/dist/query-builder.scss";
 
-import {
-  ChooseVendorsModalContainer,
-  Heading,
-  Info,
-  InfoBar,
-  InfoTextNormal,
-  StickDivider,
-  StyledButton,
-  StyledMuiLink,
-  Vendor,
-  VendorFallback,
-  VendorFallbackLogo,
-  VendorLogo,
-  VendorName,
-} from "./choose-vendors-modal.styles";
-import CustomizedSearchField from "../searchfield/searchfield.component";
+import CustomModal from "../modal/custom-modal.component";
+
+import { ChooseVendorsModalContainer, Heading, StickDivider, StyledButton } from "./choose-vendors-modal.styles.js";
+import "./choose-vendors-modal.styles.scss";
+import { QueryBuilderAntD } from "@react-querybuilder/antd";
+
+const initialQuery = { combinator: "and", rules: [] };
 
 const ChooseVendorsModal = ({ open, onClose, data }) => {
-  const [itemFilter, setItemFilter] = useState("");
+  const [query, setQuery] = useState(initialQuery);
 
-  const handleItemFilterChange = (event) => setItemFilter(event.target.value);
-
-  const generateHighlightedText = (text, filterValue) => {
-    const lowerText = text.toLowerCase();
-    const lowerFilterValue = filterValue.toLowerCase();
-
-    if (!lowerText.includes(lowerFilterValue)) {
-      return text;
-    }
-
-    const startIndex = lowerText.indexOf(lowerFilterValue);
-    const endIndex = startIndex + lowerFilterValue.length;
-
-    return (
-      <>
-        {text.substring(0, startIndex)}
-        <span style={{ backgroundColor: "#1565D8", color: "#fff" }}>{text.substring(startIndex, endIndex)}</span>
-        {text.substring(endIndex)}
-      </>
-    );
-  };
-
-  const matchingData = data.filter((item) => {
-    const vendorNameMatch = item.vendor.vendorName.toLowerCase().includes(itemFilter);
-    return vendorNameMatch;
-  });
   return (
     <CustomModal open={open} onClose={onClose} cornerPlacement>
       <ChooseVendorsModalContainer>
         <Stack spacing="20px" alignItems="center">
-          <Heading>{"Choose Bilt-in Vendors or\nAdd New One"}</Heading>
+          <Heading>Filter view Builder</Heading>
           <StickDivider />
         </Stack>
 
-        <CustomizedSearchField
-          id="searchfield-for-choose-vendor-modal"
-          fullWidth
-          placeholder="Search by Vendor Name"
-          value={itemFilter}
-          onChange={handleItemFilterChange}
-        />
-
         <Stack spacing="16px" width="100%">
-          {matchingData.map((item, index) => (
-            <InfoBar key={index}>
-              <Info>
-                <Vendor>
-                  <Box component="div" sx={{ position: "relative" }}>
-                    {item.vendor.vendorImage && item.vendor.vendorImage !== null ? (
-                      <VendorLogo src={item.vendor.vendorImage} loading="lazy" />
-                    ) : (
-                      <VendorFallback>
-                        <VendorFallbackLogo />
-                      </VendorFallback>
-                    )}
-
-                    <BadgeButton />
-                  </Box>
-                  <VendorName>{generateHighlightedText(item.vendor.vendorName, itemFilter)}</VendorName>
-                </Vendor>
-
-                <Divider sx={{ marginInline: "20px" }} orientation="vertical" flexItem />
-
-                <Stack spacing="6px">
-                  <InfoTextNormal>
-                    Phone #:&nbsp;<span style={{ fontWeight: "700" }}>{item.vendor.vendorPhone}</span>
-                  </InfoTextNormal>
-                  <InfoTextNormal>
-                    Email:&nbsp;<span style={{ fontWeight: "700" }}>{item.vendor.vendorEmail}</span>
-                  </InfoTextNormal>
-                  <InfoTextNormal>
-                    Notes:&nbsp;
-                    <span style={{ fontWeight: "700" }}>
-                      {!item.vendor.vendorNotes ? "No notes" : item.vendor.vendorNotes}
-                    </span>
-                  </InfoTextNormal>
-                </Stack>
-
-                <Divider sx={{ marginInline: "20px" }} orientation="vertical" flexItem />
-
-                <Stack spacing="6px">
-                  <InfoTextNormal>
-                    Vendor Type:&nbsp;<span style={{ fontWeight: "700" }}>{item.vendor.vendorType}</span>
-                  </InfoTextNormal>
-                  <InfoTextNormal>
-                    Amazon Cat.:&nbsp;<span style={{ fontWeight: "700" }}>{item.vendor.amazonCat}</span>
-                  </InfoTextNormal>
-                  <InfoTextNormal>
-                    Account Status:&nbsp;
-                    <span style={{ fontWeight: "700" }}>{item.vendor.accountStatus}</span>
-                  </InfoTextNormal>
-                </Stack>
-              </Info>
-            </InfoBar>
-          ))}
+          <QueryBuilderAntD>
+            <QueryBuilder fields={fields} query={query} onQueryChange={setQuery} />
+          </QueryBuilderAntD>
         </Stack>
 
         <Stack direction="row" gap="16px" justifyContent="end">
           <StyledButton variant="text" size="large" onClick={onClose}>
-            Choose Built-in Vendors
+            Cancel
           </StyledButton>
           <StyledButton
             variant="contained"
             size="large"
             endIcon={<ArrowForwardRoundedIcon sx={{ fontSize: "1.125rem !important" }} />}
           >
-            Add New Vendor
+            Create New Filter View
           </StyledButton>
         </Stack>
       </ChooseVendorsModalContainer>
--- src/components/UI/choose-vendors-modal/choose-vendors-modal.styles.js
+++ src/components/UI/choose-vendors-modal/choose-vendors-modal.styles.js
@@ -1,4 +1,4 @@
-import { Button, Link } from "@mui/material";
+import { Button } from "@mui/material";
 import styled from "styled-components";
 
 export const ChooseVendorsModalContainer = styled.div`
@@ -52,75 +52,4 @@ export const StyledButton = styled(Button)`
       }
     }
   }
-`;
-
-export const InfoBar = styled.div`
-  background: #fff;
-  border: 1px solid #C4C4C4;
-  border-radius: 10px;
-  padding: 10px;
-`;
-
-export const Info = styled.div`
-  display: flex;
-`;
-
-export const VendorLogo = styled.img`
-  position: relative;
-  min-width: 64px;
-  width: 64px;
-  min-height: 64px;
-  height: 64px;
-`;
-
-export const VendorFallback = styled.div`
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  width: 64px;
-  height: 64px;
-  border-radius: 20px;
-  background: #b3e3ff;
-`;
-
-export const VendorFallbackLogo = styled.span`
-  min-width: 40px;
-  min-height: 40px;
-  width: 40px;
-  height: 40px;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-image: url("data:image/svg+xml;charset=UTF-8,%3csvg xmlns='http://www.w3.org/2000/svg' width='40' height='41' fill='none'%3e%3cpath fill='%23fff' d='M24.444 12.722h2.223v3.89h-2.223v-3.89Z'/%3e%3cpath fill='%23fff' d='M31.667 13.278v11.111c0 .306-.25.556-.556.556H20a.557.557 0 0 1-.556-.556V13.278c0-.306.25-.556.556-.556h3.333v4.445c0 .305.25.555.556.555h3.333c.306 0 .556-.25.556-.555v-4.445h3.333c.306 0 .556.25.556.556ZM17.778 25.5H20v3.889h-2.222V25.5Z'/%3e%3cpath fill='%23fff' d='M25 26.056v11.11c0 .306-.25.556-.556.556h-11.11a.557.557 0 0 1-.556-.555V26.056c0-.306.25-.556.555-.556h3.334v4.444c0 .306.25.556.555.556h3.333c.306 0 .556-.25.556-.556V25.5h3.333c.306 0 .556.25.556.556Zm6.111-.556h2.222v3.889h-2.222V25.5Z'/%3e%3cpath fill='%23fff' d='M38.333 26.056v11.11c0 .306-.25.556-.555.556H26.667a.557.557 0 0 1-.556-.555V26.056c0-.306.25-.556.556-.556H30v4.444c0 .306.25.556.555.556h3.334c.305 0 .555-.25.555-.556V25.5h3.334c.305 0 .555.25.555.556Z'/%3e%3cpath fill='%23fff' d='M38.061 13.356 20.283 2.8a.547.547 0 0 0-.566 0L1.939 13.356a.554.554 0 1 0 .567.955l1.383-.822v23.678c0 .305.25.555.555.555h5c.306 0 .556-.25.556-.555V19.389h8.333v-6.111c0-.917.75-1.667 1.667-1.667h11.11c.917 0 1.667.75 1.667 1.667v11.667h3.333V13.489l1.383.822a.551.551 0 0 0 .761-.194.554.554 0 0 0-.194-.761ZM22.778 9.389h-5.556a.557.557 0 0 1-.555-.556c0-.305.25-.555.555-.555h5.556c.305 0 .555.25.555.555 0 .306-.25.556-.555.556Z'/%3e%3c/svg%3e");
-`;
-
-export const VendorName = styled.h2`
-  color: #4e5969;
-  font-feature-settings: "clig" off, "liga" off;
-  font-size: 1.5rem;
-  font-style: normal;
-  font-weight: 700;
-  line-height: 1.5rem;
-  text-transform: uppercase;
-`;
-
-export const Vendor = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 12px;
-`;
-
-export const InfoTextNormal = styled.p`
-  font-family: Titillium Web;
-  font-size: 0.875rem;
-  font-weight: 400;
-  line-height: 18px;
-  text-align: left;
-`;
-
-export const StyledMuiLink = styled(Link)`
-  font-family: Titillium Web !important;
-  font-size: 0.875rem !important;
-  font-weight: 600 !important;
-  line-height: 18px !important;
 `;
